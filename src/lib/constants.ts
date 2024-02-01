e41b1345d0251c7e535382af077517d1f9880fa3
Omar Flores <cerberusprogrammer@gmail.com>
2024-02-01 11:00:01 -0800

    Actualizacion

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,7 @@
 export const LINKS = {
   github: "https://github.com/CerberusProgrammer",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
+  linkedin: "https://www.linkedin.com/in/omar-flores-salazar-1a30a1238/",
+  fiverr: "https://es.fiverr.com/salazarprograms",
 };
 
 export const loaderAnimation = [
