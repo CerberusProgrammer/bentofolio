be27a1d55131477d0092612a248104a9829ae5c6
Omar Flores <cerberusprogrammer@gmail.com>
2024-02-23 07:03:06 -0800

    New integration with SolidJS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
+import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
 
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+import solidJs from "@astrojs/solid-js";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    sitemap(),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
     icon()
   ],
   markdown: {
